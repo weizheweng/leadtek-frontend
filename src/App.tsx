3e04edc5f0fb822cec24d8ad9a7fc3a42e5919d9
Zack <zack@kapito.io>
2025-04-17 22:09:30 +0800

    Update index.html and App.tsx for new project branding and routing structure; add global CSS reset and Home/Layout components

--- src/App.tsx
+++ src/App.tsx
@@ -1,41 +1,23 @@
-import { useState } from 'react'
+import { createBrowserRouter, Navigate, RouterProvider } from 'react-router-dom'
 
-import reactLogo from './assets/react.svg'
-import viteLogo from './assets/vite.svg'
-import './App.css'
+import './global.css'
+import { Home } from './pages/Home'
+import { Layout } from './pages/Layout'
 
-function App () {
-  const [count, setCount] = useState(0)
+export default function App () {
+  const router = createBrowserRouter([
+    {
+      path: '/',
+      element: <Layout />,
+      children: [
+        { path: '/', element: <Navigate to="/leadtek-frontend" replace /> },
+        { path: '/leadtek-frontend', element: <Home /> },
+        { path: '*', element: 'NotFound' },
+      ],
+    },
+  ])
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>
-        Vite + React
-      </h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is
-          {count}
-        </button>
-        <p>
-          Edit
-          <code>src/App.tsx</code>
-          and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <RouterProvider router={router} />
   )
 }
-
-export default App
---
+++ src/pages/Home.tsx
@@ -0,0 +1,9 @@
+import { Box } from '@mui/material'
+
+export function Home () {
+  return (
+    <Box p={2}>
+      123
+    </Box>
+  )
+}
